0a071a17726ab3e5339df10221dac4767577c1d8
Scott Olsen <scott.c.olsen@oracle.com>
2017-06-02 15:59:46 -0400

    (fiat): Began working on curryR

--- fiat.js
+++ fiat.js
@@ -14,19 +14,36 @@
 //Js default behavior, returns undefined.
 Function.prototype.curry = function(fn){
     let origin = this;
-    return (function(){
-      return fn(origin())();
-    }());
+    return function(){
+        return fn(origin())();
+    }
 };
 
+Function.prototype.curryR = function(fn){
+    let origin = this;
+    return function(){
+        console.log(fargs());
+        return origin(fn(fargs()))();
+    }
+};
+
+function fargs(){
+    return arguments.callee.caller.arguments.callee.arguments;
+}
 //We must execute functions all the way to the bottom of the stack
 //thus we need to provide a wrapper function for return values of our function defintions.
- const r = function(x){
+function composable(x){
     return ()=>{return x};
-};
-
-function add(a){
-    return r(a + 2);
 }
 
-console.log(add(4).curry(add));
+function add(a){
+    return composable(a + 2);
+}
+
+function multiply(a){
+    return composable(a * 6);
+}
+let f = add(4).curry(add).curry(multiply)();
+let l = add(4).curryR(add).curryR(multiply)();
+console.log(f);
+
